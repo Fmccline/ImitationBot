a40e62a5b333c0d68d6e9cdce77c969184b4056c
Frank Cline <frankmcline@gmail.com>
2019-04-17 02:17:27 -0800

    Changed output from json to csv

---
+++ src/trajectory_logger/RLBotTrajectoryConverter.py
@@ -0,0 +1,69 @@
+from RLBotTrajectoryReader import RLBotTrajectoryReader
+import datetime
+
+from RLBotTrajectoryWriter import RLBotTrajectoryWriter
+
+
+class RLBotTrajectoryConverter:
+
+    def __init__(self, log_path, json_path):
+        self.log_path = log_path
+        self.json_path = json_path
+
+    def get_bot_files(self, start_date, end_date):
+        """
+        Gets log files from the start date up to but not including the end date
+
+        :param start_date: datetime.date
+        :param end_date: datetime.date
+        :return: list of tuples (filename, bot_name)
+        """
+        bot_files = []
+        bot_names = ['ReliefBot', 'ReliefBot(2)', 'ReliefBot(3)', 'ReliefBot(4)']
+        current_date = start_date
+        while current_date != end_date:
+            for bot_name in bot_names:
+                filename = f'{self.log_path}{bot_name}-{current_date}.log'
+                bot_file = (filename, bot_name)
+                bot_files.append(bot_file)
+            current_date += datetime.timedelta(days=1)
+        return bot_files
+
+    def convert_files_to_json(self, start_date, end_date):
+        output_file = f'{self.json_path}{start_date}_{end_date}-trajectories.json'
+        bot_files = self.get_bot_files(start_date, end_date)
+
+        trajectory_reader = RLBotTrajectoryReader()
+        all_trajectories = []
+        for filename, bot_name in bot_files:
+            trajectories = trajectory_reader.get_trajectories_from_file(filename, bot_name)
+            all_trajectories.append(trajectories)
+
+        RLBotTrajectoryWriter.write_trajectories_to_json(trajectories, output_file)
+
+    def convert_files_to_csv(self, start_date, end_date):
+        output_file = f'{self.json_path}{start_date}_{end_date}-trajectories.csv'
+        bot_files = self.get_bot_files(start_date, end_date)
+
+        trajectory_reader = RLBotTrajectoryReader()
+        all_trajectories = []
+        for filename, bot_name in bot_files:
+            trajectories = trajectory_reader.get_trajectories_from_file(filename, bot_name)
+            for trajectory in trajectories:
+                all_trajectories.append(trajectory)
+
+        RLBotTrajectoryWriter.write_trajectories_to_csv(all_trajectories, output_file)
+
+
+def main():
+    log_path = 'logs/'
+    output_path = 'csv/'
+    trajectory_converter = RLBotTrajectoryConverter(log_path, output_path)
+
+    end_date = datetime.date(year=2019, month=4, day=17)
+    start_date = end_date - datetime.timedelta(1)
+    trajectory_converter.convert_files_to_csv(start_date, end_date)
+
+
+if __name__ == '__main__':
+    main()
--- src/trajectory_logger/RLBotTrajectoryWriter.py
+++ src/trajectory_logger/RLBotTrajectoryWriter.py
@@ -1,10 +1,61 @@
-import json
+import csv
+
+from Trajectory import Trajectory
 
 
 class RLBotTrajectoryWriter:
 
     @staticmethod
-    def write_trajectories_to_json(trajectories, filename):
-        as_json = json.dumps(trajectories)
-        with open(filename, "w+") as file:
-            file.write(as_json)
+    def write_trajectories_to_csv(trajectories, filename):
+        column_names = ['team',
+                        'self_score', 'self_x', 'self_y', 'self_z',
+                        'self_vx', 'self_vy', 'self_vz',
+                        'self_rx', 'self_ry', 'self_rz',
+                        'ally_score', 'ally_x', 'ally_y', 'ally_z',
+                        'ally_vx', 'ally_vy', 'ally_vz',
+                        'ally_rx', 'ally_ry', 'ally_rz',
+                        'opp1_score', 'opp1_x', 'opp1_y', 'opp_1_z',
+                        'opp1_vx', 'opp1_vy', 'opp1_vz',
+                        'opp1_rx', 'opp1_ry', 'opp1_rz',
+                        'opp_2_score', 'opp2_x', 'opp2_y', 'opp2_z',
+                        'opp2_vx', 'opp2_vy', 'opp2_vz',
+                        'opp2_rx', 'opp2_ry', 'opp2_rz',
+                        'ball0_x', 'ball0_y', 'ball0_z',
+                        'ball0_vx', 'ball0_vy', 'ball0_vz',
+                        'ball1_x', 'ball1_y', 'ball1_z',
+                        'ball1_vx', 'ball1_vy', 'ball1_vz',
+                        'ball2_x', 'ball2_y', 'ball2_z',
+                        'ball2_vx', 'ball2_vy', 'ball2_vz',
+                        'ball3_x', 'ball3_y', 'ball3_z',
+                        'ball3_vx', 'ball3_vy', 'ball3_vz',
+                        'boost0', 'boost1', 'boost2', 'boost3', 'boost4', 'boost5',
+                        'self_boost']
+
+        with open(filename, "w+", newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(column_names)
+            for trajectory in trajectories:
+                state = trajectory[Trajectory.STATE]
+                team = state[Trajectory.TEAM]
+                as_csv = [team]
+                for car_name in Trajectory.CARS:
+                    car = state[car_name]
+                    as_csv.append(car[Trajectory.SCORE])
+                    for car_state in Trajectory.CAR_STATES:
+                        for car_state_item in car[car_state]:
+                            as_csv.append(car_state_item)
+
+                ball_states = state[Trajectory.BALL]
+                for ball_name in Trajectory.BALLS:
+                    ball = ball_states[ball_name]
+                    for ball_state in Trajectory.BALL_STATES:
+                        for ball_state_item in ball[ball_state]:
+                            as_csv.append(ball_state_item)
+
+                boosts = state[Trajectory.BOOSTS]
+                for boost_state in Trajectory.BOOST_STATES:
+                    as_csv.append(boosts[boost_state])
+
+                as_csv.append(state[Trajectory.SELF_BOOST])
+                writer.writerow(as_csv)
+            print(f'Wrote {len(trajectories)} total trajectories to {filename}')

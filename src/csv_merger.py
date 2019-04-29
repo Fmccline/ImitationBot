020c86913dc6a27d0ced1cd4446c8fdb18ef5973
Frank Cline <frankmcline@gmail.com>
2019-04-28 16:16:13 -0800

    Name changes

--- src/csv_merger.py
+++ src/csv_merger.py
@@ -4,19 +4,19 @@ import glob
 
 filenames = []
 path = 'csv/'
-dates = [(14, 21), (21, 27)]
+dates = [(21, 22), (26, 27)]
 for date_range in dates:
 	filename = f'{path}2019-04-{date_range[0]}_2019-04-{date_range[1]}-trajectories.csv'
 	filenames.append(filename)
-output_file = path + '2019-04-14_2019-04-27-trajectories.csv'
+output_file = f'{path}2019-04-{dates[0][0]}_2019-04-{dates[-1][1]}-trajectories.csv'
 
-li = []
+data_frames = []
 for filename in filenames:
 	print(f'Reading from {filename}')
 	df = pd.read_csv(filename, index_col=None, header=0)
-	li.append(df)
+	data_frames.append(df)
 
-frame = pd.concat(li, axis=0, ignore_index=True)
+frame = pd.concat(data_frames, axis=0, ignore_index=True)
 print(f'Writing to {output_file}')
 frame.to_csv(output_file)
 print('Finished.')
